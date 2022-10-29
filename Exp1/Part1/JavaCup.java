4b6f39823d0375c78268a92b7fe3803debb033b1
yasaman <45336465+yasmansh@users.noreply.github.com>
2022-10-30 01:59:12 +0330

    Add Part 1 Files

---
+++ Exp1/Part1/JavaCup.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JavaCup {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press number1: ");
+        int i = scanner.nextInt();
+        System.out.println("Press number2: ");
+        int j = scanner.nextInt();
+        System.out.println("Press number3: ");
+        int k = scanner.nextInt();
+        temp();
+        eval(i, j, k);
+    }
+
+    public static void eval(int i, int j, int k) {
+        if (i * i + j * j == k * k || i * i == j * j + k * k || j * j == i * i + k * k) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public static void temp() {
+        int[] a = new int[200000000];
+        int k = 0;
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 0; j < 20000; j++) {
+                a[k] = (i + j);
+                k++;
+            }
+        }
+    }
+}
